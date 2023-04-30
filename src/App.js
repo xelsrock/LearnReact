e9fa588163792d23a5db8696fd8ba1363ac65a84
xelsrock <alex.perevosnicov@gmail.com>
2023-04-30 18:37:07 +0300

    css

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import React, { useState } from "react";
 import Counter from "./components/Counter.jsx";
+import ClassCounter from "./components/ClassCounter.jsx";
+import './styles/App.css';
 
 function App() {
   const [value, setValue] = useState('Текст в инпуте')
 
   return (
     <div className="App">
-      <Counter/>
+      <div className="post">
+        <div className="post__content">
+          <strong>1. JavaScript</strong>
+          <div>
+            JavaScript - язык прогламмирования
+          </div>
+        </div>
+        <div className="post__btns">
+            <button>Удалить</button>
+        </div>
+      </div>
     </div>
   )
 }
